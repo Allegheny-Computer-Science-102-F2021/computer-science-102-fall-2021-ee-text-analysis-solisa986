d5aa70aed94ea6debc49e52fc08f6bada2a8baf1
solisa986 <solisa@allegheny.edu>
2021-11-05 18:29:51 -0400

    added implementation of two functions in extract.py

--- textanalysis/textanalysis/extract.py
+++ textanalysis/textanalysis/extract.py
@@ -1,12 +1,22 @@
 """Extract the paragraphs and other textual content from the paragraphs of text."""
 
 # Add the required imports at the top of the file
+from typing import List
+from typing import Set
 
+import re
+
+NEWLINES_RE = re.compile(r"\n{2,}")
 
 def extract_lines_including_blanks(input_lines: str) -> List[str]:
     """Extract all of the lines, including the blanks lines."""
     # extract all of the lines in the file, using splitlines
+    text = []
+    split_text = input_lines.splitlines()
+    for line in split_text:
+        text.append(line)
     # return all of the extracted lines
+    return text
 
 
 def extract_lines_not_including_blanks(input_lines: str) -> List[str]:
@@ -18,14 +28,18 @@ def extract_lines_not_including_blanks(input_lines: str) -> List[str]:
 
 def extract_paragraphs(input_lines: str) -> List[str]:
     """Extract all of the paragraphs from the lines of textual input."""
-    # implement a function that uses, for instance, regular expressions to
-    # break up a provided text in a string as a list of strings, with each
-    # string representing a paragraph
     # consult the following reference:
     # https://stackoverflow.com/questions/53240763/python-how-to-separate-paragraphs-from-text
     # you may use other online tutorials or responses in your implementation
     # of this function as long as you provide the needed references
     # remove leading and trailing newline characters
+    no_newlines = input_lines.strip("\n")
+    # implement a function that uses, for instance, regular expressions to
+    # break up a provided text in a string as a list of strings, with each
+    # string representing a paragraph
+    split_text = NEWLINES_RE.split(no_newlines)
+    paragraphs = [p + "\n" for p in split_text if p.strip()]
+    return paragraphs
 
 
 def extract_unique_words_paragraphs(paragraphs: List[str]) -> List[Set[str]]:
